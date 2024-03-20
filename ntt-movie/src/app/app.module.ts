4b699b13711ff902dc55cea767d946632207c7e3
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-03-20 11:55:37 +0000

    Adicionando html do content

--- ntt-movie/src/app/app.module.ts
+++ ntt-movie/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContentComponent
+    ContentComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
