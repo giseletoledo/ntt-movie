64bc3b71207219e66a4a8c5c775152aec039ad1e
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-03-20 11:55:37 +0000

    Adicionando html do content

--- ntt-movie/src/app/content/content.component.ts
+++ ntt-movie/src/app/content/content.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
-
+  movies = [
+    { 
+      imageUrl: 'https://br.web.img3.acsta.net/pictures/17/05/29/23/31/530814.jpg', 
+      title: 'Homem-Aranha', 
+      description: 'Description of Spiderman' 
+    },
+    { 
+      imageUrl: 'https://down-br.img.susercontent.com/file/8a907199c70c1a18a9447ebb3d1d7e82', 
+      title: 'Vingadores', 
+      description: 'Description of Movie 2' 
+    },
+    { 
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU1b01CCN5miKrlHg7pSb__D8kwGtG8jEgYw&usqp=CAU', 
+      title: 'Marvels', 
+      description: 'Description of Movie 3' 
+    },
+    { 
+      imageUrl: 'path/to/image4.jpg', 
+      title: 'Movie Title 4', 
+      description: 'Description of Movie 4' 
+    }
+  ];
 }
