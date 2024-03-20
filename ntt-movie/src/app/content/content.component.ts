f93b02591748db08931d3735b4cd93c2e9921a8c
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-03-20 12:46:59 +0000

    alterando imagens

--- ntt-movie/src/app/content/content.component.ts
+++ ntt-movie/src/app/content/content.component.ts
@@ -13,17 +13,17 @@ export class ContentComponent {
       description: 'Em Homem-Aranha: De Volta ao Lar, depois de atuar ao lado dos Vingadores, chegou a hora de Peter Parker (Tom Holland) voltar para casa e para a sua vida, já não mais tão normal.' 
     },
     { 
-      imageUrl: 'https://down-br.img.susercontent.com/file/8a907199c70c1a18a9447ebb3d1d7e82', 
+      imageUrl: 'https://br.web.img3.acsta.net/pictures/19/04/26/17/30/2428965.jpg', 
       title: 'Vingadores', 
       description: 'Em Vingadores: Ultimato, após Thanos eliminar metade das criaturas vivas em Vingadores: Guerra Infinita, os heróis precisam lidar com a dor da perda de amigos e seus entes queridos. ' 
     },
     { 
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU1b01CCN5miKrlHg7pSb__D8kwGtG8jEgYw&usqp=CAU', 
+      imageUrl: 'https://lumiere-a.akamaihd.net/v1/images/image_2a250270.png', 
       title: 'Marvels', 
       description: 'Capitã Marvel, Ms. Marvel e Monica Rambeau estão envolvidas em um misterioso fenômeno em que seus poderes estão interligados, fazendo com que elas troquem de lugar sem entender a causa para tal.' 
     },
     { 
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7TOAzebl6wAkJpZ0opPn6syrWPhcOkk9hSw&usqp=CAU', 
+      imageUrl: 'https://br.web.img3.acsta.net/pictures/16/09/29/21/15/495786.jpg', 
       title: 'Doutor Estranho', 
       description: 'Em Doutor Estranho no Multiverso da Loucura, após derrotar Dormammu e enfrentar Thanos nos eventos de Vingadores: Ultimato, o Mago Supremo, Stephen Strange (Benedict Cumberbatch), e seu parceiro Wong (Benedict Wong), continuam suas pesquisas sobre a Joia do Tempo.' 
     }
