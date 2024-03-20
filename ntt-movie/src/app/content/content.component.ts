68d19ddfc4e115ff46937cad882a7789051f5334
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-03-20 12:33:48 +0000

    Adicionando conteudo

--- ntt-movie/src/app/content/content.component.ts
+++ ntt-movie/src/app/content/content.component.ts
@@ -10,22 +10,22 @@ export class ContentComponent {
     { 
       imageUrl: 'https://br.web.img3.acsta.net/pictures/17/05/29/23/31/530814.jpg', 
       title: 'Homem-Aranha', 
-      description: 'Description of Spiderman' 
+      description: 'Em Homem-Aranha: De Volta ao Lar, depois de atuar ao lado dos Vingadores, chegou a hora de Peter Parker (Tom Holland) voltar para casa e para a sua vida, já não mais tão normal.' 
     },
     { 
       imageUrl: 'https://down-br.img.susercontent.com/file/8a907199c70c1a18a9447ebb3d1d7e82', 
       title: 'Vingadores', 
-      description: 'Description of Movie 2' 
+      description: 'Em Vingadores: Ultimato, após Thanos eliminar metade das criaturas vivas em Vingadores: Guerra Infinita, os heróis precisam lidar com a dor da perda de amigos e seus entes queridos. ' 
     },
     { 
       imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU1b01CCN5miKrlHg7pSb__D8kwGtG8jEgYw&usqp=CAU', 
       title: 'Marvels', 
-      description: 'Description of Movie 3' 
+      description: 'Capitã Marvel, Ms. Marvel e Monica Rambeau estão envolvidas em um misterioso fenômeno em que seus poderes estão interligados, fazendo com que elas troquem de lugar sem entender a causa para tal.' 
     },
     { 
-      imageUrl: 'path/to/image4.jpg', 
-      title: 'Movie Title 4', 
-      description: 'Description of Movie 4' 
+      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7TOAzebl6wAkJpZ0opPn6syrWPhcOkk9hSw&usqp=CAU', 
+      title: 'Doutor Estranho', 
+      description: 'Em Doutor Estranho no Multiverso da Loucura, após derrotar Dormammu e enfrentar Thanos nos eventos de Vingadores: Ultimato, o Mago Supremo, Stephen Strange (Benedict Cumberbatch), e seu parceiro Wong (Benedict Wong), continuam suas pesquisas sobre a Joia do Tempo.' 
     }
   ];
 }
