5febde6b052c69b9cc85c875ee25227bbe01a9e4
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-03-21 13:51:40 +0000

    criando componente favorito

---
+++ ntt-movie/src/app/favorite/favorite.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrl: './favorite.component.css'
+})
+export class FavoriteComponent {
+  @Input() isFavorite: boolean = false;
+  @Input() imdbID: string = '';
+
+  toggleFavorite(): void {
+    this.isFavorite = !this.isFavorite;
+    this.saveFavoriteState();
+  }
+
+  saveFavoriteState(): void {
+    const favoriteData = {
+      imdbID: this.imdbID,
+      isFavorite: this.isFavorite,
+    };
+    localStorage.setItem('favoriteMovies', JSON.stringify(favoriteData));
+  }
+}
