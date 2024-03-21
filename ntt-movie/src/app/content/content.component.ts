cc17856c158e0f147d633db835740ed195f96b2a
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-03-21 04:18:21 +0000

    ajustes

--- ntt-movie/src/app/content/content.component.ts
+++ ntt-movie/src/app/content/content.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Movie } from '../core/movie';
+import { MovieService } from '../movie.service';
+
 
 @Component({
   selector: 'app-content',
@@ -6,26 +9,32 @@ import { Component } from '@angular/core';
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
-  movies = [
-    { 
-      imageUrl: 'https://br.web.img3.acsta.net/pictures/17/05/29/23/31/530814.jpg', 
-      title: 'Homem-Aranha', 
-      description: 'Em Homem-Aranha: De Volta ao Lar, depois de atuar ao lado dos Vingadores, chegou a hora de Peter Parker (Tom Holland) voltar para casa e para a sua vida, já não mais tão normal.' 
-    },
-    { 
-      imageUrl: 'https://br.web.img3.acsta.net/pictures/19/04/26/17/30/2428965.jpg', 
-      title: 'Vingadores', 
-      description: 'Em Vingadores: Ultimato, após Thanos eliminar metade das criaturas vivas em Vingadores: Guerra Infinita, os heróis precisam lidar com a dor da perda de amigos e seus entes queridos. ' 
-    },
-    { 
-      imageUrl: 'https://lumiere-a.akamaihd.net/v1/images/image_2a250270.png', 
-      title: 'Marvels', 
-      description: 'Capitã Marvel, Ms. Marvel e Monica Rambeau estão envolvidas em um misterioso fenômeno em que seus poderes estão interligados, fazendo com que elas troquem de lugar sem entender a causa para tal.' 
-    },
-    { 
-      imageUrl: 'https://br.web.img3.acsta.net/pictures/16/09/29/21/15/495786.jpg', 
-      title: 'Doutor Estranho', 
-      description: 'Em Doutor Estranho no Multiverso da Loucura, após derrotar Dormammu e enfrentar Thanos nos eventos de Vingadores: Ultimato, o Mago Supremo, Stephen Strange (Benedict Cumberbatch), e seu parceiro Wong (Benedict Wong), continuam suas pesquisas sobre a Joia do Tempo.' 
+  movies: Movie[] | undefined;
+  movieTitle: string = ''; 
+  defaultPosterUrl: string = 'assets/default-poster.png'; 
+  
+
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit(): void {
+    this.fetchMovie();
+  }
+
+ 
+
+  fetchMovie(): void {
+    if (this.movieTitle) {
+      this.movieService.getMovieByTitle(this.movieTitle).subscribe(
+        movies => {
+          this.movies = movies.map(movie => ({
+            ...movie,
+            Poster: movie.Poster !== 'N/A' ? movie.Poster : this.defaultPosterUrl
+          }));
+        },
+        error => {
+          console.error('Erro ao buscar filmes:', error);
+        }
+      );
     }
-  ];
+  }
 }
