b9657c297ab540640768c760813c32de13d9c8e9
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-03-21 04:59:48 +0000

    trocando plot por year

--- ntt-movie/src/app/content/content.component.ts
+++ ntt-movie/src/app/content/content.component.ts
@@ -2,7 +2,6 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { Movie } from '../core/movie';
 import { MovieService } from '../movie.service';
 
-
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -11,6 +10,7 @@ import { MovieService } from '../movie.service';
 export class ContentComponent {
   movies: Movie[] | undefined;
   movieTitle: string = ''; 
+  movieDescription: string = ''; 
   defaultPosterUrl: string = 'assets/default-poster.png'; 
   
 
@@ -20,8 +20,6 @@ export class ContentComponent {
     this.fetchMovie();
   }
 
- 
-
   fetchMovie(): void {
     if (this.movieTitle) {
       this.movieService.getMovieByTitle(this.movieTitle).subscribe(
@@ -30,6 +28,7 @@ export class ContentComponent {
             ...movie,
             Poster: movie.Poster !== 'N/A' ? movie.Poster : this.defaultPosterUrl
           }));
+          console.log(this.movies)
         },
         error => {
           console.error('Erro ao buscar filmes:', error);
