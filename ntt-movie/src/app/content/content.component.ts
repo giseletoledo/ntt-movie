71bc0caa4a799281fc2988fbe5fbce2176d5d7a2
Gisele <88586474+giseletoledo@users.noreply.github.com>
2024-03-22 00:46:19 +0000

    Adicionando os favoritos do localStorage

--- ntt-movie/src/app/content/content.component.ts
+++ ntt-movie/src/app/content/content.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Movie } from '../core/movie';
 import { MovieService } from '../movie.service';
+import { FavoritesService } from '../favorites.service';
 
 @Component({
   selector: 'app-content',
@@ -8,32 +9,47 @@ import { MovieService } from '../movie.service';
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
-  movies: Movie[] | undefined;
+  movies: Movie[] = [];
   movieTitle: string = ''; 
   movieDescription: string = ''; 
   defaultPosterUrl: string = 'assets/default-poster.png'; 
+  isFilterActive: boolean = false;
+  displayMovies: Movie[] = [];
   
-
-  constructor(private movieService: MovieService) { }
+  constructor(private movieService: MovieService, private favoritesService: FavoritesService) { }
 
   ngOnInit(): void {
     this.fetchMovie();
   }
+  
   fetchMovie(): void {
     if (this.movieTitle) {
       this.movieService.getMovieByTitle(this.movieTitle).subscribe(
         movies => {
-          // Added conditional check
           this.movies = movies ? movies.map(movie => ({
             ...movie,
             Poster: movie.Poster !== 'N/A' ? movie.Poster : this.defaultPosterUrl
           })) : [];
-          console.log(this.movies);
+          this.updateDisplayMovies();
         },
         error => {
           console.error('Erro ao buscar filmes:', error);
         }
       );
     }
+  }
+
+  updateDisplayMovies(): void {
+    if (this.isFilterActive) {
+      const favoriteIDs = this.favoritesService.getFavoriteIDs();
+      this.displayMovies = this.movies.filter(movie => favoriteIDs.includes(movie.imdbID));
+    } else {
+      this.displayMovies = this.movies;
+    }
+  }
+
+  toggleFavoritesFilter(): void {
+    this.isFilterActive = !this.isFilterActive;
+    this.updateDisplayMovies();
   }
 }
--- ntt-movie/src/app/favorite/favorite.component.ts
+++ ntt-movie/src/app/favorite/favorite.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { FavoritesService } from '../favorites.service';
 
 @Component({
   selector: 'app-favorite',
@@ -8,17 +9,17 @@ import { Component, Input } from '@angular/core';
 export class FavoriteComponent {
   @Input() isFavorite: boolean = false;
   @Input() imdbID: string = '';
-  
-  toggleFavorite(): void {
-    this.isFavorite = !this.isFavorite;
-    this.saveFavoriteState();
-  }
 
-  saveFavoriteState(): void {
-    const favoriteData = {
-      imdbID: this.imdbID,
-      isFavorite: this.isFavorite,
-    };
-    localStorage.setItem('favoriteMovies', JSON.stringify(favoriteData));
+
+  constructor(private favoritesService: FavoritesService) {}
+
+toggleFavorite(): void {
+  this.isFavorite = !this.isFavorite;
+  if (this.isFavorite) {
+    this.favoritesService.addToFavorites(this.imdbID);
+  } else {
+    this.favoritesService.removeFromFavorites(this.imdbID);
   }
+}
+
 }
